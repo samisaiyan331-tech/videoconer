6064612eb234fa3f46ffbdd5741f66488fb130ce
samisaiyan331-tech <samisaiyan331@gmail.com>
2026-06-07 02:27:37 +0600

    a27 Update content.js

--- content.js
+++ content.js
@@ -1,23 +1,167 @@
 // content.js - কন্টেন্ট ডাটা সোর্স (টাইটেল ছাড়া)
 const posts = [
-    {
-        id: "1",
-        thumbnail: "https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?q=80&w=500&auto=format&fit=crop",
-        videoLink: "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4"
+    /*
+        {
+        id: "a2",
+        thumbnail: "",
+        videoLink: ""
+    },
+
+    */
+         {
+        id: "a27",
+        thumbnail: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/a76b7aac-fa67-4348-a887-7c03278464e7.jpg",
+        videoLink: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/78fcd73c-9f8b-475d-83fc-1f0bbe22925c.mp4"
+    },
+         {
+        id: "a26",
+        thumbnail: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/17c19210-886c-4174-9f59-5be306da4fb2.jpg",
+        videoLink: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/0bc0d7fe-eda7-47aa-8d3f-e35ee641aa61.mp4"
+    },
+           {
+        id: "a25",
+        thumbnail: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/5777482f-922c-4a98-aa88-6785c080c42e.jpg",
+        videoLink: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/69afcdaa-0f6d-4d38-bca0-fe70cd1fca95.mp4"
+    },
+          {
+        id: "a24",
+        thumbnail: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/9c094422-996e-4c12-bb56-fc99807eb47f.jpg",
+        videoLink: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/7b5ccd31-7bd8-413a-ba43-4b17bd569f20.mp4"
+    },
+           {
+        id: "a23",
+        thumbnail: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/244f3b27-19dc-42cc-bd7c-2c3aa5678410.jpg",
+        videoLink: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/e01af4b7-7b76-4bd4-a72b-d6ddbf4122b3.mp4"
+    },
+           {
+        id: "asa2",
+        thumbnail: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/48234df7-09c8-4787-868a-ea7bdb677df4.jpg",
+        videoLink: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/0f14b139-00f1-4c77-aae2-2f2bc1ee3925.mp4"
+    },
+            {
+        id: "a22",
+        thumbnail: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/ef2a346a-4879-4fa0-8a0e-237581389889.jpg",
+        videoLink: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/9ccbc170-5fb3-423c-8029-df6f5f13b626.mp4"
+    },
+          {
+        id: "a21",
+        thumbnail: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/1000007217.jpg",
+        videoLink: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/9f51a2e2-577e-4144-8f92-c833b692e061.mp4"
+    },
+           {
+        id: "a20",
+        thumbnail: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/a61b0d61-5767-44a2-a813-0eef7114f941.jpg",
+        videoLink: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/6b9a1de1-d9c0-4a36-a13c-8440a663445c.mp4"
+    },
+       {
+        id: "a19",
+        thumbnail: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/603f0163-940a-4690-9ebd-783c13db6dcf.jpg",
+        videoLink: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/d21dfe93-53f3-48b9-affa-72454f7b85d4.mp4"
     },
     {
-        id: "2",
-        thumbnail: "https://images.unsplash.com/photo-1579783900882-c0d3dad7b119?q=80&w=500&auto=format&fit=crop",
-        videoLink: "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4"
+        id: "a18",
+        thumbnail: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/cf237c28-2de6-44c5-9169-3c60d2cb17eb.jpg",
+        videoLink: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/abd89a2b-581f-4f6f-8094-8b56fd49314a.mp4"
     },
     {
-        id: "3",
-        thumbnail: "https://images.unsplash.com/photo-1618005198143-d8c80327f394?q=80&w=500&auto=format&fit=crop",
-        videoLink: "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4"
+        id: "a17",
+        thumbnail: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/3d3cac19-87ae-47da-a6c4-b6d3a5513215.jpg",
+        videoLink: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/videoplayback_122.mp4"
+    },
+
+    {
+        id: "a16",
+        thumbnail: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/db0a3f67-8e92-4159-a4d2-f4dc87d97e52.jpg",
+        videoLink: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/27fccdfb-e6d8-4dda-be37-a2872d3bf770.mp4"
+    },
+
+        {
+        id: "a15",
+        thumbnail: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/20e477b4-c27c-4e3c-8942-c77f6586327f.jpg",
+        videoLink: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/videoplayback_116.mp4"
+    },
+            {
+        id: "a14",
+        thumbnail: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/36f3c400-f765-4328-9a93-0bb4a66bd14b.jpg",
+        videoLink: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/e6369ce8-e422-4aac-9be6-f25dfefe42b1.mp4"
+    },
+            {
+        id: "a13",
+        thumbnail: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/a6742b40-12ca-4647-adf1-bf743bea289f.jpg",
+        videoLink: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/70ce1dd6-4e7b-4c93-a4db-1d4873b2eb86.mp4"
+    },
+           {
+        id: "a12",
+        thumbnail: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/fb7190a4-89db-4da2-a2ad-419479def8d3.jpg",
+        videoLink: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/5b602e9c-ead0-4e35-85b2-cba13f256200.mp4"
+    },
+
+            {
+        id: "a11",
+        thumbnail: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/95b718c6-bbe3-436a-863f-6ffaf152ab9e.jpg",
+        videoLink: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/3f35777f-eb4c-4949-b835-ddc53de99380.mp4"
+    },
+
+            {
+        id: "a10",
+        thumbnail: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/4a377469-5b5c-4214-b283-d74e0315498b.jpg",
+        videoLink: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/videoplayback_101.mp4"
+    },
+           {
+        id: "a9",
+        thumbnail: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/9983951e-67b7-4626-8512-ed60af7fdeb0.jpg",
+        videoLink: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/videoplayback_100.mp4"
+    },
+
+            {
+        id: "a8",
+        thumbnail: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/xxxxx%20xx.jpg",
+        videoLink: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/961e594e-7806-427d-b32d-bb04d13fb51a.mp4"
+    },
+
+            {
+        id: "a7",
+        thumbnail: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/f908ec69-a5d3-45ce-8113-53d7fa8d06df.jpg",
+        videoLink: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/videoplayback_96.mp4"
+    },
+        {
+        id: "aa2",
+        thumbnail: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/photo_6271583831026503508_y.jpg",
+        videoLink: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/3f16709f-1216-43f1-ba17-325b6e5e457a.mp4"
+    },
+         {
+        id: "a6",
+        thumbnail: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/image%20(1).png",
+        videoLink: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/ca57a91d-a5d9-476b-ba6d-ff4db8a59223.mp4"
+    },
+        {
+        id: "a5",
+        thumbnail: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/de614db0-9aca-4bd6-b070-4452c4a65d64.jpg",
+        videoLink: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/7eafb063-9fa5-40a3-b29f-ac365888c041.mp4"
     },
     {
-        id: "4",
-        thumbnail: "https://images.unsplash.com/photo-1604871000636-074fa5117945?q=80&w=500&auto=format&fit=crop",
-        videoLink: "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4"
+        id: "a4",
+        thumbnail: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/7eb20e92-781d-455e-a031-6299cb77af94.jpg",
+        videoLink: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/99b15216-c7aa-42bc-9551-547d4459c372.mp4"
+    },
+    {
+        id: "a3",
+        thumbnail: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/image.png",
+        videoLink: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/videoplayback_82.mp4"
+    },
+    {
+        id: "a2",
+        thumbnail: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/e463da7a-8e52-4cbc-88bd-183539e40757.jpg",
+        videoLink: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/e1f734e3-7282-41d0-b007-5553cb0e3bb2.mp4"
+    },
+    {
+        id: "a1",
+        thumbnail: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/f8ef4848-1a46-4864-9fef-e19f0682008e.jpg",
+        videoLink: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/e60ff621-59e2-40c4-bf74-aa9b9b9fded3.mp4"
+    },
+      {
+        id: "aas2",
+        thumbnail: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/photo_6208506377949351672_x.jpg",
+        videoLink: "https://file.garden/aiHcyz3Ry0OdnlAf/vc/6-6/videoplayback_145.mp4"
     }
 ];
