24d0f0fe871024d82c9ecbd50c4ee8c35e45b8f2
samisaiyan331-tech <samisaiyan331@gmail.com>
2026-06-06 17:42:06 +0600

    Opne

---
+++ content.js
@@ -0,0 +1,23 @@
+// content.js - কন্টেন্ট ডাটা সোর্স (টাইটেল ছাড়া)
+const posts = [
+    {
+        id: "1",
+        thumbnail: "https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?q=80&w=500&auto=format&fit=crop",
+        videoLink: "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4"
+    },
+    {
+        id: "2",
+        thumbnail: "https://images.unsplash.com/photo-1579783900882-c0d3dad7b119?q=80&w=500&auto=format&fit=crop",
+        videoLink: "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4"
+    },
+    {
+        id: "3",
+        thumbnail: "https://images.unsplash.com/photo-1618005198143-d8c80327f394?q=80&w=500&auto=format&fit=crop",
+        videoLink: "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4"
+    },
+    {
+        id: "4",
+        thumbnail: "https://images.unsplash.com/photo-1604871000636-074fa5117945?q=80&w=500&auto=format&fit=crop",
+        videoLink: "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4"
+    }
+];
